7c9b9e4a1dbf862aec85e9c2295f5316f7c3a39e
85piccio <85piccio@gmail.com>
2015-03-05 13:38:05 +0100

    bk and utils

--- src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/trianglecount/TriangleCountPlusPlus.java
+++ src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/trianglecount/TriangleCountPlusPlus.java
@@ -17,20 +17,19 @@
  */
 package it.uniroma1.bdc.tesi.piccioli.giraphstandalone.trianglecount;
 
+import it.uniroma1.bdc.tesi.piccioli.giraphstandalone.message.MessageLongIdLondValue;
 import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.Vertex;
 import org.apache.hadoop.io.NullWritable;
 import java.io.IOException;
 import org.apache.giraph.edge.Edge;
-import org.apache.hadoop.io.Text;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import java.util.Set;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 
 @SuppressWarnings("rawtypes")
-public class TriangleCountPlusPlus extends BasicComputation<LongWritable, LongWritable, NullWritable, Text> {
+public class TriangleCountPlusPlus extends BasicComputation<LongWritable, LongWritable, NullWritable, MessageLongIdLondValue> {
 
     /**
      * Somma aggregator name
@@ -40,48 +39,49 @@ public class TriangleCountPlusPlus extends BasicComputation<LongWritable, LongWr
     /* SOLO GRAFO NON DIRETTO ? */
     @Override
     public void compute(Vertex<LongWritable, LongWritable, NullWritable> vertex,
-            Iterable<Text> messages) throws IOException {
+            Iterable<MessageLongIdLondValue> messages) throws IOException {
 
         Iterable<Edge<LongWritable, NullWritable>> edges = vertex.getEdges();
 
         if (getSuperstep() == 0) {
             //calcolo degree e invio a vertici vicini
-            LongWritable degree = new LongWritable (Iterables.size(edges));
-            vertex.setValue(degree); 
+            LongWritable degree = new LongWritable(Iterables.size(edges));
+            vertex.setValue(degree);
 
             for (Edge<LongWritable, NullWritable> edge : edges) {
-                this.sendMessage(edge.getTargetVertexId(), new Text(vertex.getId().toString() + "-" + degree.toString()));
+                this.sendMessage(edge.getTargetVertexId(), new MessageLongIdLondValue(vertex.getId(), degree));
             }
 
         } else if (getSuperstep() == 1) {
-            for (Text message : messages) {
-                String[] splitMsg = message.toString().split("-");
-                if (splitMsg.length > 0 /*&& !(vertex.getId().toString().equals("") || vertex.getValue().toString().equals(""))*/) {
+            for (MessageLongIdLondValue message : messages) {
+//                String[] splitMsg = message.toString().split("-");
+//                if (splitMsg.length > 0 /*&& !(vertex.getId().toString().equals("") || vertex.getValue().toString().equals(""))*/) {
 
-                    int messageValue = Integer.parseInt(splitMsg[1]);
-                    int vertexValue = Integer.parseInt(vertex.getValue().toString());
-                    DoubleWritable messageId = new DoubleWritable(Double.parseDouble(splitMsg[0]));
-                    DoubleWritable vertexId =  new DoubleWritable(Double.parseDouble(vertex.getId().toString()));
+                long messageValue = message.getValue().get();
+                long vertexValue = vertex.getValue().get();
+                LongWritable messageId = message.getId();
+                LongWritable vertexId = vertex.getId();
 
-                    if ((messageValue < vertexValue) || ((messageValue == vertexValue) && (messageId.compareTo(vertexId) < 0))) {
-                        this.removeEdgesRequest(new LongWritable(Long.parseLong(splitMsg[0])), vertex.getId());
-                    }
+//                    if ((messageValue < vertexValue) || ((messageValue == vertexValue) && (messageId.compareTo(vertexId) < 0))) {
+                if ((messageValue < vertexValue) || ((messageValue == vertexValue) && (messageId.get() < vertexId.get()))) {
+                    this.removeEdgesRequest(messageId, vertexId);
                 }
+//                }
             }
         } else if (getSuperstep() == 2) {
             for (Edge<LongWritable, NullWritable> edge : edges) {
-                this.sendMessageToAllEdges(vertex, new Text(edge.getTargetVertexId().toString()));
+                this.sendMessageToAllEdges(vertex, new MessageLongIdLondValue(edge.getTargetVertexId(), new LongWritable(Long.MAX_VALUE)));
             }
         } else if (getSuperstep() == 3) {
             Integer T = 0;
-            Set<String> edgeMap = Sets.<String>newHashSet();
+            Set<LongWritable> edgeMap = Sets.<LongWritable>newHashSet();
 
             for (Edge<LongWritable, NullWritable> edge : edges) {
-                edgeMap.add(edge.getTargetVertexId().toString());
+                edgeMap.add(edge.getTargetVertexId());
             }
 
-            for (Text message : messages) {
-                if (edgeMap.contains(message.toString())) {
+            for (MessageLongIdLondValue message : messages) {
+                if (edgeMap.contains(message.getId())) {
                     T++;
                 }
             }
