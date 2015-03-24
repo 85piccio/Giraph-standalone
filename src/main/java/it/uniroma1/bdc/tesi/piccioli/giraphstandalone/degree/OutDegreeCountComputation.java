d43254cc02a56eeee62cc898015bad9d13dbbf90
85piccio <85piccio@gmail.com>
2015-03-24 13:19:33 +0100

    bk e ref

--- src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/degree/OutDegreeCountComputation.java
+++ src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/degree/OutDegreeCountComputation.java
@@ -1,20 +1,18 @@
 package it.uniroma1.bdc.tesi.piccioli.giraphstandalone.degree;
 
 import org.apache.giraph.graph.BasicComputation;
-import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.Vertex;
 
 import java.io.IOException;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 
-public class OutDegreeCountComputation extends BasicComputation<Text, LongWritable, NullWritable, Text> {
+public class OutDegreeCountComputation extends BasicComputation<LongWritable, LongWritable, NullWritable, NullWritable> {
 
     @Override
     public void compute(
-            Vertex<Text, LongWritable, NullWritable> vertex,
-            Iterable<Text> messages) throws IOException {
+            Vertex<LongWritable, LongWritable, NullWritable> vertex,
+            Iterable<NullWritable> messages) throws IOException {
 
         vertex.setValue(new LongWritable(vertex.getNumEdges()));
         vertex.voteToHalt();
