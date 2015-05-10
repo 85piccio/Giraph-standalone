0697adc0633e096350e9c0b8bf9604f2728856b9
85piccio <85piccio@gmail.com>
2015-05-10 16:29:43 +0200

    commit prec

--- src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/output/DenseSubgraphDirectTextOutputFormat.java
+++ src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/output/DenseSubgraphDirectTextOutputFormat.java
@@ -17,7 +17,7 @@
  */
 package it.uniroma1.bdc.tesi.piccioli.giraphstandalone.output;
 
-import it.uniroma1.bdc.tesi.piccioli.giraphstandalone.densesubgraph.direct.DenseSubgraphDirectVertexValue;
+import it.uniroma1.bdc.tesi.piccioli.giraphstandalone.densesubgraph.direct.longwritable.DenseSubgraphDirectVertexValue;
 import org.apache.giraph.graph.Vertex;
 import org.apache.giraph.io.formats.TextVertexOutputFormat;
 import org.apache.hadoop.io.NullWritable;
