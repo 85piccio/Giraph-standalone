129d60bcd74ab7aaeab4ab5533cbb99b8518cd87
85piccio <85piccio@gmail.com>
2015-06-11 10:18:55 +0200

    fix corretto ordinamento algoritmo

--- src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/trianglecountplusplus/intwritable/twophases/VertexComputePhase1.java
+++ src/main/java/it/uniroma1/bdc/tesi/piccioli/giraphstandalone/trianglecountplusplus/intwritable/twophases/VertexComputePhase1.java
@@ -29,58 +29,57 @@ import org.apache.hadoop.io.IntWritable;
  *  INPUT FORMAT - IntIntNullNullInputFormat.java 
  *  OUTPUT FORMAT - VertexWithIntValueNullEdgeTextOutputFormat.java
  */
-
 @SuppressWarnings("rawtypes")
 public class VertexComputePhase1 extends BasicComputation<IntWritable, IntWritable, NullWritable, MessageIntIdIntValue> {
 
     /**
      * Somma aggregator name
      */
-
-    
-    /**	Prima fase composta da i primi 2 superstep
-     * 1 superstep - calcolo del degree di ogni nodo e invio info a nodi vicino
-     * 2 superstep - elimino archi fuori ordinamento 
-     * 
+    /**
+     * Prima fase composta da i primi 2 superstep 1 superstep - calcolo del
+     * degree di ogni nodo e invio info a nodi vicino 2 superstep - elimino
+     * archi fuori ordinamento
+     *
      * @param vertex
      * @param messages
      * @throws java.io.IOException
-     **/
+     *
+     */
     @Override
     public void compute(Vertex<IntWritable, IntWritable, NullWritable> vertex,
-	    Iterable<MessageIntIdIntValue> messages) throws IOException {
+            Iterable<MessageIntIdIntValue> messages) throws IOException {
 
-	Iterable<Edge<IntWritable, NullWritable>> edges = vertex.getEdges();
+        Iterable<Edge<IntWritable, NullWritable>> edges = vertex.getEdges();
 
-	if (getSuperstep() == 0) {
-	    //calcolo degree e invio a vertici vicini
-	    IntWritable degree = new IntWritable(vertex.getNumEdges());
-	    vertex.setValue(degree);
+        if (getSuperstep() == 0) {
+            //calcolo degree e invio a vertici vicini
+            IntWritable degree = new IntWritable(vertex.getNumEdges());
+            vertex.setValue(degree);
 
-	    for (Edge<IntWritable, NullWritable> edge : edges) {
-		this.sendMessage(edge.getTargetVertexId(), new MessageIntIdIntValue(vertex.getId(), degree));
-	    }
+            for (Edge<IntWritable, NullWritable> edge : edges) {
+                this.sendMessage(edge.getTargetVertexId(), new MessageIntIdIntValue(vertex.getId(), degree));
+            }
 
-	} else if (getSuperstep() == 1) {
+        } else if (getSuperstep() == 1) {
 
-	    //Ricevo Degree dai nodi vicini, elimino edge che collegano nodi "< degree minori"
-	    IntWritable vertexId = vertex.getId();
-	    IntWritable vertexValue = vertex.getValue();
+            //Ricevo Degree dai nodi vicini, elimino edge che collegano nodi "< degree minori"
+            IntWritable vertexId = vertex.getId();
+            IntWritable vertexValue = vertex.getValue();
 
-	    IntWritable messageId;
-	    IntWritable messageValue;
+            IntWritable messageId;
+            IntWritable messageValue;
 
-	    for (MessageIntIdIntValue message : messages) {
+            for (MessageIntIdIntValue message : messages) {
 
-		messageValue = message.getValue();
-		messageId = message.getId();
+                messageValue = message.getValue();
+                messageId = message.getId();
 
-		if ((messageValue.compareTo(vertexValue) < 0)
-			|| ((messageValue.compareTo(vertexValue) == 0) && (messageId.compareTo(vertexId) < 0))) {
-		    this.removeEdgesRequest(messageId, vertexId);
-		}
-	    }
-	}
+                if ((messageValue.compareTo(vertexValue) > 0)
+                        || ((messageValue.compareTo(vertexValue) == 0) && (messageId.compareTo(vertexId) < 0))) {
+                    this.removeEdgesRequest(messageId, vertexId);
+                }
+            }
+        }
     }
 
 }
